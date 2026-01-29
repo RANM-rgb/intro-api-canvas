b0b1066e059e2599b78d6e781426075038edd64b
Rodrigo Noeggerath <sonrodrigo.15@gmail.com>
2026-01-29 14:33:46 -0600

    Ejercicio Terminado

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// cuadro verde en la esquina superior izquierda del canvas
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);
